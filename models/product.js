4d9766c4225819221fbb053dbbca7e497d242854
matheuslcandido <matheus.l.candido@gmail.com>
2020-02-01 19:15:18 -0300

    adding the data and models of the products

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+  constructor(id, ownerId, title, imgUrl, description, price) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.title = title;
+    this.imgUrl = imgUrl;
+    this.description = description;
+    this.price = price;
+  };
+};
