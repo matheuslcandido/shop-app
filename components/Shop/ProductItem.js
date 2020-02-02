5c846296fd7a741d6af6ce3b9ed91b1c05d8bfcf
matheuslcandido <matheus.l.candido@gmail.com>
2020-02-02 16:29:33 -0300

    changing the component ProductItem to receive the click on the entire component using the touchableOpacity and the TouchableNativeFeedback

--- components/Shop/ProductItem.js
+++ components/Shop/ProductItem.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import { StyleSheet, View, Text, Image, Button, TouchableOpacity, TouchableNativeFeedback, Platform, TouchableNativeFeedbackBase } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
 const ProductItem = props => {
+  let TouchableComponent = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableComponent = TouchableNativeFeedback;
+  };
+
   return (
     <View style={styles.product}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: props.image }} />
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price}>{props.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.actions}>
-        <Button 
-          color={Colors.primary} 
-          title='View Details' 
-          onPress={props.onViewDetail} 
-        />
-        <Button 
-          color={Colors.primary} 
-          title='To Cart' 
-          onPress={props.onAddToCard} 
-        />
+      <View style={styles.touchable}>
+        <TouchableComponent onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>{props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button 
+                color={Colors.primary} 
+                title='View Details' 
+                onPress={props.onViewDetail} 
+              />
+              <Button 
+                color={Colors.primary} 
+                title='To Cart' 
+                onPress={props.onAddToCard} 
+              />
+            </View>
+          </View>
+        </TouchableComponent>
       </View>
     </View>
   );
@@ -40,6 +52,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     height: 300,
     margin: 20,
+    overflow: 'hidden',
+  },
+  touchable: {
+    overflow: 'hidden',
+    borderRadius: 10,
   },
   imageContainer: {
     width: '100%',
