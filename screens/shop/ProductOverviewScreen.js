dda7ab8f50db2f4938bd19de4c92ac0c6cc3e591
matheuslcandido <matheus.l.candido@gmail.com>
2020-02-02 15:21:47 -0300

    using the component ProductItem to render the items of the FlatList of products

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/Shop/ProductItem';
+
+const ProductOverViewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+
+  return (
+    <FlatList 
+      data={products} 
+      keyExtractor={item => item.id} 
+      renderItem={itemData => 
+        <ProductItem 
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCard={() => {}}
+        />
+      }
+    />
+  );
+};
+
+ProductOverViewScreen.navigationOptions = {
+  headerTitle: 'All Products',
+};
+
+export default ProductOverViewScreen;
