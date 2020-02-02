fec674fc692ee2e3f6c268c57e16fac539296ebf
matheuslcandido <matheus.l.candido@gmail.com>
2020-02-02 15:18:25 -0300

    fixing the name of the property imageUrl

--- models/product.js
+++ models/product.js
@@ -1,10 +1,12 @@
 class Product {
-  constructor(id, ownerId, title, imgUrl, description, price) {
+  constructor(id, ownerId, title, imageUrl, description, price) {
     this.id = id;
     this.ownerId = ownerId;
     this.title = title;
-    this.imgUrl = imgUrl;
+    this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
   };
 };
+
+export default Product;
